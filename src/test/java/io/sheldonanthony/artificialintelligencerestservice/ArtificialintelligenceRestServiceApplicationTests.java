ee361f28c0ff179744eb256ea55d42a4565bb3d9
Sheldon Anthony <shakes082@gmail.com>
2021-01-30 12:05:14 +0200

    Changes...

--- src/test/java/io/sheldonanthony/artificialintelligencerestservice/ArtificialintelligenceRestServiceApplicationTests.java
+++ src/test/java/io/sheldonanthony/artificialintelligencerestservice/ArtificialintelligenceRestServiceApplicationTests.java
@@ -1,7 +1,32 @@
 package io.sheldonanthony.artificialintelligencerestservice;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+ * Copyright (c) 2020, Sheldon Anthony
+ * All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the Apache License, Version 2.0 only, as
+ * published by the Free Software Foundation.  
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the Apache License, Version 2.0
+ * for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the Apache License, Version 2.0
+ * along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sheldon Anthony, PostNet Suite 129,
+ * Private Bag X 1510, Glenvista, 2058, Johannesburg, South Africa
+ * or visit https://shakes082.github.io/sheldonanthonyio.github.io/
+ * if you need additional information
+ *  or have any questions.
+ */
 
 @SpringBootTest
 class ArtificialintelligenceRestServiceApplicationTests {
